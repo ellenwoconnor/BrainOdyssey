15a9cf6f187cfb5f2e4365234be56fe80204e996
Ellen O'Connor <eoconnor9484@gmail.com>
2015-10-29 17:39:01 -0700

    Models created and db is ready to be seeded using seed.py

---
+++ model.py
@@ -0,0 +1,190 @@
+"""Models and database functions for Brain Odyssey project"""
+
+
+from flask_sqlalchemy import SQLAlchemy
+
+
+db = SQLAlchemy()
+
+###########################################################################
+# Model definitions
+
+class Location(db.Model):
+    """An individual pinpoint location in the brain represented by an x-y-z
+    coordinate (each an integer between 0-100)."""
+
+    __tablename__ = "locations"
+
+    location_id = db.Column(db.Integer, autoincrement=True, primary_key=True)
+    x_coord = db.Column(db.Float, nullable=False)
+    y_coord = db.Column(db.Float, nullable=False)
+    z_coord = db.Column(db.Float, nullable=False)
+    label = db.Column(db.String, nullable=True)
+    space = db.Column(db.String(30), nullable=True)  # MNI, TAL or Unknown
+
+    @classmethod
+    def check_by_xyz_space(cls, x=None, y=None, z=None, space=None):
+        """Returns  existing xyz instance of the class (None if no such
+        instance exists)."""
+
+        location_obj = cls.query.filter(cls.x_coord == x,
+                                        cls.y_coord == y,
+                                        cls.z_coord == z,
+                                        cls.space == space).first()
+        return location_obj
+
+    def __repr__(self):
+        """Displays info about a location."""
+        return "<Location id=%d x=%d y=%d z=%d label=%s space=%s" % (
+            self.location_id, self.x_coord, self.y_coord,
+            self.z_coord, self.label, self.space)
+
+
+class Activation(db.Model):
+    """Peak activation coordinate reported in a study."""
+
+    __tablename__ = "activations"
+
+    activation_id = db.Column(db.Integer, autoincrement=True, primary_key=True)
+    pmid = db.Column(db.Integer, db.ForeignKey('studies.pmid'), nullable=False)
+    location_id = db.Column(db.Integer, db.ForeignKey('locations.location_id'),
+                                                      nullable=False)
+
+    # relationships
+    study = db.relationship('Study', backref="activations")
+    location = db.relationship('Location', backref="activations")
+
+    def __repr__(self):
+        """Displays info about an activation."""
+
+        return "<Activation pmid=%d space=%s" % (self.pmid, self.location_id)
+
+
+class Study(db.Model):
+    """An individual study by PubMed ID."""
+
+    __tablename__ = "studies"
+
+    pmid = db.Column(db.Integer, primary_key=True)
+    doi = db.Column(db.String(200))
+    title = db.Column(db.String(300))
+    authors = db.Column(db.String(300))
+    year = db.Column(db.Integer)
+    journal = db.Column(db.String(200))
+
+    @classmethod
+    def get_study_by_pmid(cls, pmid):
+        """Returns existing instance of Study class associated
+        with a PubMed ID."""
+
+        study_obj = cls.query.filter(cls.pmid == pmid).first()
+        return study_obj
+
+    def __repr__(self):
+        """Displays info about a study."""
+
+        return "<Study pmid=%d doi=%s title=%s year=%d" % (
+            self.pmid, self.doi, self.title, self.year)
+
+
+class StudyTerm(db.Model):
+    """A relationship between a term and a study."""
+
+    __tablename__ = "studies_terms"
+
+    studyterm_id = db.Column(db.Integer, autoincrement=True, primary_key=True)
+    word = db.Column(db.String(100), db.ForeignKey('terms.word'))
+    pmid = db.Column(db.Integer, db.ForeignKey('studies.pmid'), nullable=False)
+    # A value between 0-1 representing the relative frequency that the keyword
+    # is mentioned in a study.
+    frequency = db.Column(db.Float, nullable=False)
+
+    term = db.relationship('Term', backref="studies_terms")
+    study = db.relationship('Study', backref="studies_terms")
+
+    def __repr__(self):
+        """Displays info about a word associated with a study."""
+        return "<StudyTerm word=%s pmid=%d frequency=%f" % (
+            self.word, self.pmid, self.frequency)
+
+
+class Term(db.Model):
+    """A term extracted from study text."""
+
+    __tablename__ = "terms"
+
+    word = db.Column(db.String(100), primary_key=True)
+
+    @classmethod
+    def check_for_term(cls, word):
+        """Returns True if a term is in Term table already, False if not."""
+
+        if cls.query.filter(cls.word == word).first() is None:
+            return False
+        else:
+            return True
+
+    def __repr__(self):
+        """Displays info about a term."""
+
+        return "<Terms term=%s" % (self.term)
+
+
+class TermCluster(db.Model):
+    """An association between a term and a topic cluster."""
+
+    __tablename__ = "terms_clusters"
+
+    termcluster_id = db.Column(db.Integer, autoincrement=True, primary_key=True)
+    word = db.Column(db.String(100), db.ForeignKey('terms.word'))
+    cluster_id = db.Column(db.Integer, db.ForeignKey('clusters.cluster_id'))
+
+    term = db.relationship('Term', backref="terms_clusters")
+    cluster = db.relationship('Cluster', backref="terms_clusters")
+
+    def __repr__(self):
+        """Displays info about a term-cluster association."""
+
+        return "<TermCluster id=%d term=%s cluster_id=%d" % (
+            self.termcluster_id, self.term, self.cluster_id)
+
+
+class Cluster(db.Model):
+    """A topic cluster, identified by an integer from 0-200."""
+
+    __tablename__ = "clusters"
+
+    cluster_id = db.Column(db.Integer, primary_key=True)
+
+    @classmethod
+    def check_for_cluster(cls, cluster_id):
+        """Returns True if a cluster_id is already in the table, False if not."""
+
+        if cls.query.filter(cls.cluster_id == cluster_id).first() is None:
+            return False
+        else:
+            return True
+
+    def __repr__(self):
+        """Displays info about a cluster."""
+
+        return "<Cluster id=%s" % (self.id)
+
+
+##############################################################################
+# Helper functions
+
+
+def connect_to_db(app):
+    """Connect the database to our Flask app."""
+
+    # Configure to use our SQLite database
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///odyssey.db'
+    db.app = app
+    db.init_app(app)
+
+
+if __name__ == "__main__":
+    from server import app
+    connect_to_db(app)
+    print "Connected to DB."
---
+++ seed.py
@@ -0,0 +1,180 @@
+"""Utility file to seed database from files in seed_data directory"""
+
+
+from model import Location, Activation, Study, StudyTerm, Term, TermCluster, Cluster
+from model import connect_to_db, db
+from server import app
+
+
+def load_studies():
+    """Load data from database.txt into Location, Activation, Study tables."""
+
+    # Delete all rows in existing tables, so if we need to run this a second time,
+    # we won't add duplicates
+    Location.query.delete()
+    Study.query.delete()
+    Activation.query.delete()
+
+    skip = True
+    count_studies = 0
+
+    # Parse txt file and convert to appropriate data types for seeding
+    for row in open("seed_data/database.txt"):
+
+        # Skip the header of the txt file
+        if skip:
+            skip = False
+            continue
+
+        # Stop after the first 5000 rows for now
+        if count_studies > 5000:
+            break
+
+        row = row.rstrip().split('\t')
+
+        # Information to go into Study, if applicable:
+        pmid = int(row[0])
+        doi = row[1]
+        title = row[9]
+        authors = row[10]
+        year = int(row[11])
+        journal = row[12].rstrip()
+
+        # Information to go into Location, if applicable
+        x = float(row[2])
+        y = float(row[3])
+        z = float(row[4])
+        space = row[5]
+
+        # Check whether PMID is already in Study; if not, add it to db.
+        study_obj = Study.get_study_by_pmid(pmid)
+
+        if study_obj is None:
+            study_to_add = Study(pmid=pmid, doi=doi, title=title,
+                                 authors=authors, year=year, journal=journal)
+            db.session.add(study_to_add)
+            db.session.commit()
+
+        # Check whether xyz is already in Location; if not, add it to db and
+        # retrieve its location ID (an autoincrementing primary key).
+        # If xyz already in Location, retrieve its location_id.
+        location_obj = Location.check_by_xyz_space(x, y, z, space)
+
+        if location_obj is None:
+            location_to_add = Location(x_coord=x, y_coord=y, z_coord=z,
+                                       space=space)
+            db.session.add(location_to_add)
+            db.session.commit()
+            loc_id = Location.check_by_xyz_space(x, y, z, space).location_id
+        else:
+            loc_id = location_obj.location_id
+
+        # Add activation to db, using location_id identified/generated above
+        activation_to_add = Activation(pmid=pmid, location_id=loc_id)
+        db.session.add(activation_to_add)
+        db.session.commit()
+
+        # Print where we are and increment counter
+        print "Database.txt seeding row ", count_studies
+        count_studies += 1
+
+def load_studies_terms():
+    """Load info from studies_terms.txt into StudyTerm & Term tables."""
+
+    print "Studies_terms.txt seeding"
+
+    # Delete all rows in existing tables, so if we need to run this a second time,
+    # we won't be trying to add duplicate users
+    StudyTerm.query.delete()
+    Term.query.delete()
+
+    skip = True
+    count_studies_terms = 0
+
+    for row in open("seed_data/studies_terms.txt"):
+        # Skip the first line of the file
+        if skip:
+            skip = False
+            continue
+
+        # Stop after 5000 lines
+        if count_studies_terms > 5000:
+            break
+
+        # Parse txt file and convert to appropriate data types for seeding
+        row = row.rstrip().split('\t')
+
+        # If the term starts with "X", it is not a word but a number, e.g. "X01"
+        # These don't make sense to track, so skip these rows.
+        if row[2].startswith('\"X'):
+            continue
+
+        # Skip the lines indicating that a term did not appear anywhere
+        # in the article (frequency of 0)
+        if float(row[3]) == 0.0:
+            continue
+
+        pmid = int(row[1])
+        word = row[2].strip('\"')
+        freq = float(row[3])
+
+        # Check if the word is already in Term; if not, add it
+        if Term.check_for_term(word) is False:
+            word_to_add = Term(word=word)
+            db.session.add(word_to_add)
+
+        # Add the row to the studies_terms table
+        studies_terms_to_add = StudyTerm(word=word, pmid=pmid, frequency=freq)
+        db.session.add(studies_terms_to_add)
+        db.session.commit()
+
+        # Print where we are and increment counter
+        print "studies_terms.txt seeding row ", count_studies_terms
+        count_studies_terms += 1
+
+
+def load_clusters():
+    """Load info from topics.txt file into Cluster, TermCluster tables"""
+
+    # Delete whatever's in the db already
+    Cluster.query.delete()
+    TermCluster.query.delete()
+
+    count_clusters = 0
+
+    for row in open('topics.csv'):
+
+        row = row.rstrip().split(',')
+
+        # Parse the txt into the appropriate data types for seeding
+        cluster = int(row[1][-3:])
+        word = row[3].strip()
+
+        # Check if word is in our list of key terms. If it is, add to
+        # TermCluster table to allow for lookup later
+        if Term.check_for_term(word) is True:
+            term_cluster_to_add = TermCluster(word=word, cluster_id=cluster)
+            db.session.add(term_cluster_to_add)
+            db.session.commit()
+
+        # Check if a cluster is in our list of clusters. If it's not, add it.
+        if Cluster.check_for_cluster(cluster) is False:
+            cluster_to_add = Cluster(cluster_id=cluster)
+            db.session.add(cluster_to_add)
+            db.session.commit()
+
+        # Print where we are and increment counter
+        print "Topics.txt seeding row", count_clusters
+
+        count_clusters += 1
+
+if __name__ == "__main__":
+    connect_to_db(app)
+
+    # In case tables haven't been created, create them
+    db.create_all()
+
+    # Import different types of data
+    load_studies()
+    load_studies_terms()
+    load_clusters()
